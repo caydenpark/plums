6ee7de992e4db6c6a4732d0d53da501437df2ac2
kerokero014 <kevinjrm.kr@gmail.com>
2024-05-14 15:32:14 -0600

    refactor: Update layout to include Header and Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
         <Header />
-
+        {children}
+        
         <Footer />
       </body>
     </html>
