823bf66ea034a9343befd741a458daf9fd718f22
kerokero014 <kevinjrm.kr@gmail.com>
2024-06-03 15:39:18 -0600

    refactor: Update Topics page components and layout

---
+++ src/app/components/TopicsBoard.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Image from "next/image";
+import AddTopic from "./AddTopic";
+import Link from "next/link";
+import { useState } from "react";
+import SingleCard from "./SingleCard";
+
+export default function TopicsBoard() {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleClick = () => {
+    setShowForm(true);
+  };
+
+  const handleClose = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <div className=" grid cursor-pointer grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <SingleCard />
+      <SingleCard />
+      <SingleCard />
+      <SingleCard />
+
+      <div>
+        <button
+          className="mt-4 rounded-full bg-blue-100 p-4 text-white shadow-lg transition-colors duration-200 hover:bg-slate-400 fixed bottom-0 right-0 m-5"
+          onClick={handleClick}
+        >
+          <Image
+            src="/add_icon.png"
+            alt="Add a new topic"
+            width={35}
+            height={35}
+          />
+        </button>
+        {showForm && <AddTopic onClose={handleClose} />}
+      </div>
+    </div>
+  );
+}
