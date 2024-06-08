be2985e7db1bed67b71efafc3df96dc671d111a7
kerokero014 <kevinjrm.kr@gmail.com>
2024-06-07 19:27:08 -0600

    api and id page

---
+++ src/app/Topics/[id]/page.tsx
@@ -0,0 +1,11 @@
+import MainHeader from "@/app/components/MainHeader";
+
+
+export default function Page() {
+    return (
+        <>
+        <MainHeader />
+        <div>Topics</div>
+        </>
+    );
+}
