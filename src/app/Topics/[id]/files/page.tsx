1455fb6ae86768eaa9b961f1fab12b13aa1b4715
kerokero014 <kevinjrm.kr@gmail.com>
2024-07-05 14:16:38 -0600

    feat: Add FilePage and ImagePage components

---
+++ src/app/Topics/[id]/files/page.tsx
@@ -0,0 +1,7 @@
+export default function FilePage() {
+  return (
+    <div>
+      <h1>Page for Files</h1>
+    </div>
+  );
+}
---
+++ src/app/Topics/[id]/images/page.tsx
@@ -0,0 +1,7 @@
+export default function ImagePage() {
+  return (
+    <div>
+      <h1>Page for Images</h1>
+    </div>
+  );
+}
