212164f8d5d8f98c4c3ffc14a0dff59775cd1fd7
kerokero014 <kevinjrm.kr@gmail.com>
2024-07-11 21:40:09 -0600

    Refactor cn.ts and csn.ts files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,47 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { WavyBackground } from "./components/WavyBackground";
 
 export default function Home() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <Image
+              src="/Logo.png"
+              alt="PLUMS Logo"
+              width={150}
+              height={150}
+              className="m-auto"
+            />
 
-      {/* Background Image Start */}
-      <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        ></div>
-      </div>
+            <h1 className="mt-5 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              PLUMS
+            </h1>
+            <p className="mt-9 text-xl font-bold leading-8 text-gray-800">
+              Personal Learning Management System
+            </p>
 
-      {/* End*/}
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <Link
+                href="/add-topics"
+                className="rounded-md bg-indigo-500 px-3.5 py-3 text-sm font-semibold text-white shadow-xl hover:bg-indigo-300 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add Topic
+              </Link>
 
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
-        <div className="text-center">
-          <Image
-            src="/Logo.png"
-            alt="PLUMS Logo"
-            width={150}
-            height={150}
-            className="m-auto"
-          />
-
-          <h1 className="mt-5 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            PLUMS
-          </h1>
-          <p className="mt-9 text-lg leading-8 text-gray-600">
-            Personal Learning Management System
-          </p>
-
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-              href="/add-topics"
-              className="rounded-md bg-indigo-500 px-3.5 py-3 text-sm font-semibold text-white shadow-xl hover:bg-indigo-300 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Add Topic
-            </Link>
-
-            <Link
-              href="/Topics"
-              className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold leading-6 text-white shadow-xl hover:bg-indigo-300 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              View Topics
-            </Link>
+              <Link
+                href="/Topics"
+                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold leading-6 text-white shadow-xl hover:bg-indigo-300 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                View Topics
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-
-      {/* Background Image Start */}
-      <div
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        ></div>
-      </div>
-      {/* Background Image END */}
-      
-    </div>
+    </WavyBackground>
   );
 }
