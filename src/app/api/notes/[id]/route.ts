180c6999bdffc2dfb637c84bad62120da969ad21
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-07-20 14:54:24 -0600

    Now able to update a note using the "Edit" button

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -25,3 +25,31 @@ export async function DELETE(
     );
   }
 }
+
+// PUT /api/topics/[id]/notes/[id]
+// function to update a single note in the database based on its id
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const { title, content, topic_id } = await req.json();
+
+  try {
+    const updatedNote = await prisma.note.update({
+      where: { id: parseInt(id, 10) },
+      data: {
+        title,
+        content,
+        topic_id,
+      },
+    });
+    return NextResponse.json(updatedNote, { status: 200 });
+  } catch (error) {
+    console.error("Error updating note:", error);
+    return NextResponse.json(
+      { error: "Failed to update note" },
+      { status: 500 }
+    );
+  }
+}
